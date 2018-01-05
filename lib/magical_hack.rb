2f58cba1c5daf68253a6cc930f9f0d421466122f
Felix Steiny <felix@thelevelup.com>
2018-01-05 15:06:48 -0500

    Concat JS

--- lib/magical_hack.rb
+++ lib/magical_hack.rb
@@ -9,4 +9,19 @@ module MagicalHack
     file_paths.join ''
   end
 
+  def self.concat_js_scripts
+    file_paths = Dir['./lib/public/js/**/*.js']
+
+    result = ''
+
+    file_paths.each do |file|
+      file = File.open(file, 'r').read
+      file.each_line do |line|
+        result << line
+      end
+    end
+
+    result
+  end
+
 end
--- lib/routes.rb
+++ lib/routes.rb
@@ -6,3 +6,8 @@ URL_BASE = '/MagicalHack'
 get URL_BASE do
   slim :main, :layout => false
 end
+
+get URL_BASE + '/js' do
+  content_type 'text/javascript'
+  MagicalHack.concat_js_scripts
+end
